d7706aac6b1315849276c2aca12e292b08cc4725
Rida Ennaciri <rennacir@e3r9p10.1337.ma>
2024-02-05 16:02:13 +0100

    cpp08

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,33 +1,21 @@
 #include "MutantStack.hpp"
 #include <list>
 
-
-// int main()
-// {
-// 	MutantStack<int> mstack;
-// 	mstack.push(153);
-// 	mstack.push(151);
-// 	mstack.push(152);
-// 	mstack.pop();
-// 	std::cout << mstack.top();
-// }
-
 int main()
 {
-
-std::list<int> mstack;
-mstack.push_back(5);
-mstack.push_back(17);
-// std::cout << mstack.top() << std::endl;
-mstack.pop_back();
+MutantStack<int> mstack;
+mstack.push(5);
+mstack.push(17);
+std::cout << mstack.top() << std::endl;
+mstack.pop();
 std::cout << mstack.size() << std::endl;
-mstack.push_back(3);
-mstack.push_back(5);
-mstack.push_back(737);
+mstack.push(3);
+mstack.push(5);
+mstack.push(737);
 //[...]
-mstack.push_back(0);
-std::list<int>::iterator it = mstack.begin();
-std::list<int>::iterator ite = mstack.end();
+mstack.push(0);
+MutantStack<int>::iterator it = mstack.begin();
+MutantStack<int>::iterator ite = mstack.end();
 ++it;
 --it;
 while (it != ite)
@@ -35,6 +23,6 @@ while (it != ite)
 std::cout << *it << std::endl;
 ++it;
 }
-// std::stack<int> s(mstack);
+std::stack<int> s(mstack);
 return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stack>
 
 int get_min(std::vector<int> v)
 {
-	// if ()
 	int min = v[0];
 	for (unsigned long i = 0; i < v.size() ; i++)
 	{
@@ -29,25 +29,14 @@ int shortestSpan(std::vector<int> vec)
 	return shortest;
 }
 
-
-// int main()
-// {
-// 	// std::vector<int> vec;
-
-// 	// vec.push_back(1);
-// 	// vec.push_back(8);
-// 	// vec.push_back(10);
-// 	// vec.push_back(2);
-// 	// std::cout << shortestSpan(vec);
-// 	for (int i = 0; i < vec.size() ; i++)
-// 		std::cout << vec[i] << std::endl;
-// 	// for (int i = 0; i < tmp.size() ; i++)
-// 	// 	std::cout << tmp[i] << std::endl;
-// }
-#include <stack>
+class hmed
+{
+	public :
+		int a;
+};
 
 int main()
 {
-	std::stack<>
+	hmed g;
 	return 0;
 }
